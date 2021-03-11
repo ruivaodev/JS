c11026153b1b1ac0b412b4642efda70e80b5a9f7
schirmann <cschirmann.dev@gmail.com>
2021-03-11 19:04:59 -0300

    Manipulaçao de arrays

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ let lista = ["ovo", "corante", "farinha", "massa"];
 // lista.pop(remova o ultimo item da lista)
 //lista.shift(remove o primei item da lista)
 //lista.push(adiciona mais itens ao array)
+//lista.sort( ordena o array em ordem alfabetica)
+
+// metodo de remoção de itens dentro de um array
+//lista.splice(1, 1); seleciona a posição do item a ser removido no primeiro parametro
+// depois a quantidade de itens a serem removidos do array
 let res = lista;
